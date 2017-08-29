42785372f3e4ac271406dea5b07dac61279762c1
Aravinthraj <aravinthrajmsp@outlook.com>
2017-08-29 15:22:35 +0530

    Added interger literals program
    Added intergerliterals.cpp under basic_language_elements

---
+++ basic_language_elements/integerliterals.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "int data type literals" << endl;
+    cout << "------------------------" << endl;
+    cout << "Decimal literals" << endl;
+    cout << "decimal         12 : " << 12 <<endl;
+    cout << "decimal       -159 : " << -159 <<endl;
+    cout <<endl;
+    cout <<"Octal literals" <<endl;
+    cout <<"octal          014 : "<< 014 <<endl;
+    cout <<"octal        -0237 : " << -0237 <<endl;
+    cout <<endl;
+    cout <<"Hexadecimal literals" <<endl;
+    cout <<"hexadecimal    0xc : " << 0xc <<endl;
+    cout <<"hexadecimal  -0x9f : " << -0x9f <<endl;
+    cout <<"hexadecimal    0xC : " << 0xC <<endl;
+    cout <<"hexadecimal  -0x9F : " << -0x9F <<endl;
+    cout <<"hexadecimal    0Xc : " << 0Xc <<endl;
+    cout <<"hexadecimal  -0X9f : " << -0X9f <<endl;
+    cout <<"hexadecimal    0XC : " << 0XC <<endl;
+    cout <<"hexadecimal  -0X9F : " << -0X9F <<endl;
+    cout <<endl;
+    cout <<"Binary literals" <<endl;
+    cout <<"binary      0b1100 : " << 0b1100 <<endl;
+    cout <<"binary -0b10011111 : " << -0b10011111 <<endl;
+    cout <<"binary      0B1100 : " << -0B1100 <<endl;
+    cout <<"binary -0B10011111 : " << -0B10011111 <<endl;
+    return 0 ;
+}
