45abe9fc095a999978bc40f514674dd0327b9d54
Aravinthraj <aravinthrajmsp@outlook.com>
2017-08-29 15:21:19 +0530

    Added Floating Point For Zero program
    Added FloatingPointForZero.cpp under basic_language_elements

---
+++ basic_language_elements/FloatingPointForZero.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "0.0d      : " << 0.0d <<endl;
+    cout << "0.0D      : " << 0.0D <<endl;
+    cout << "0D        : " << 0D <<endl;
+    cout << "0.0f       : " << 0.0f <<endl;
+    cout << "0.0F      : " << 0.0F <<endl;
+    cout << "0f         : " << 0f  <<endl;
+}
