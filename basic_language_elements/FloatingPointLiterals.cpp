63db976735b5212c8ad0037879fa86a18ce9f68a
Aravinthraj <aravinthrajmsp@outlook.com>
2017-08-29 15:19:31 +0530

    Added Floating Point Literals Program
    Added FloatingPointLiterals.cpp under basic_language_elements

---
+++ basic_language_elements/FloatingPointLiterals.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "double data type literals" << endl;
+    cout << "------------------------" << endl;
+    cout << "0.0d       : " << 0.0d <<endl;
+    cout << "0.0D      : " << 0.0D <<endl;
+    cout << ".1122d     : " << .1122d <<endl;
+    cout << "45.d       : " << 45.d <<endl;
+    cout << "45.D      : " << 45.D <<endl;
+    cout << ".123D     : " << .123D <<endl;
+    cout << ".123d     : " << .123d <<endl;
+    cout << "1.754E+2d  : " << 1.754E+2d <<endl;
+    cout << ".732E+1D  : " << .732E+1D <<endl;
+    cout <<endl;
+    cout << "float data type literals" <<endl;
+    cout << "------------------------" <<endl;
+    cout << "0.0f      : " << 0.0f <<endl;
+    cout << "0.0F      : " << 0.0F <<endl;
+    cout << ".1122f    : " << .1122f <<endl;
+    cout << "45.f      : " << 45.f  <<endl;
+    cout << "45.F      : " << 45.F <<endl;
+    cout << ".123F     : " << .123F  <<endl;
+    cout << ".123f     : " << .123f <<endl;
+    cout << "1.754E+2f : " << 1.754E+2f <<endl;
+    cout << ".732E+1f  : " << .732E+1f <<endl;
+    return 0 ;
+}
