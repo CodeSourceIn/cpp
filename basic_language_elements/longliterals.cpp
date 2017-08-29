ec97e78172888bda35c349fb170c71e6ee1eaf10
Aravinthraj <aravinthrajmsp@outlook.com>
2017-08-29 15:23:28 +0530

    Added long literals program
    Added longliterals.cpp under basic_language_elements

---
+++ basic_language_elements/longliterals.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "long data type literals" << endl;
+    cout << "------------------------" << endl;
+    cout << "Decimal literals" << endl;
+    cout << "decimal         12l : " << 12l <<endl;
+    cout << "decimal       -159l : " << -159l <<endl;
+    cout << "decimal         12L : " << 12L <<endl;
+    cout << "decimal       -159L : " << -159L <<endl;
+    cout <<endl;
+    cout <<"Octal literals" <<endl;
+    cout <<"octal          014l : "<< 014l <<endl;
+    cout <<"octal        -0237l : " << -0237l <<endl;
+    cout <<"octal          014L : "<< 014L <<endl;
+    cout <<"octal        -0237L : " << -0237L <<endl;
+    cout <<endl;
+    cout <<"Hexadecimal literals" <<endl;
+    cout <<"hexadecimal    0xcl : " << 0xcl <<endl;
+    cout <<"hexadecimal  -0x9fl : " << -0x9fl <<endl;
+    cout <<"hexadecimal    0xCl : " << 0xCl <<endl;
+    cout <<"hexadecimal  -0x9Fl : " << -0x9Fl <<endl;
+    cout <<"hexadecimal    0Xcl : " << 0Xcl <<endl;
+    cout <<"hexadecimal  -0X9fl : " << -0X9fl <<endl;
+    cout <<"hexadecimal    0XCl : " << 0XCl <<endl;
+    cout <<"hexadecimal  -0X9Fl : " << -0X9Fl <<endl;
+    cout <<"hexadecimal    0xcL : " << 0xcL <<endl;
+    cout <<"hexadecimal  -0x9fL : " << -0x9fL <<endl;
+    cout <<"hexadecimal    0xCL : " << 0xCL <<endl;
+    cout <<"hexadecimal  -0x9FL : " << -0x9FL <<endl;
+    cout <<"hexadecimal    0XcL : " << 0XcL <<endl;
+    cout <<"hexadecimal  -0X9fL : " << -0X9fL <<endl;
+    cout <<"hexadecimal    0XCL : " << 0XCL <<endl;
+    cout <<"hexadecimal  -0X9FL : " << -0X9FL <<endl;
+    cout <<endl;
+    cout <<"Binary literals" <<endl;
+    cout <<"binary      0b1100l : " << 0b1100l <<endl;
+    cout <<"binary -0b10011111l : " << -0b10011111l <<endl;
+    cout <<"binary      0B1100l : " << 0B1100l <<endl;
+    cout <<"binary -0B10011111l : " << -0B10011111l <<endl;
+    cout <<"binary      0b1100L : " << 0b1100L <<endl;
+    cout <<"binary -0b10011111L : " << -0b10011111L <<endl;
+    cout <<"binary      0B1100L : " << 0B1100L <<endl;
+    cout <<"binary -0B10011111L : " << -0B10011111L <<endl;
+    return 0 ;
+}
